65613f968adeb7126901673bd34933720456f26f
José Reyes <lab@jreyes.es>
2018-05-16 10:02:51 +0200

    Primeros pasos

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require("request");
+var config = require("./config");
+
+class Bot{
+  init(TOKEN){
+    return new Promise((resolve, reject) => {
+      let url = `https://api.telegram.org/bot${TOKEN}/getMe`
+      request(url, (error, r, body) => {
+        const response = JSON.parse(body).result
+        if(error) return
+        if(!response) return
+        this.id = response.id || ''
+        this.first_name = response.first_name || ''
+        this.last_name = response.last_name || ''
+        this.username = response.username || ''
+        this.language_code = response.language_code || ''
+        resolve()
+      })
+    })
+   }
+
+   getName(){
+    if(this.last_name){
+      return `${this.first_name} ${this.last_name}`
+    }
+    else{
+      return `${this.first_name}`
+    }
+   }
+
+   introduceYourself(){
+    console.log(`Hola, mi nombre es ${this.getName()}. Puedes hablar conmigo usando mi nombre de usuario: @${this.username}`);
+   }
+}
+
+TOKEN = '109901133:AAE1xhD8gsis_YDCUDYqGps8fmasZyuBul8'
+const bot = new Bot()
+bot.init(config.TOKEN).then(() => {
+ bot.introduceYourself()
+})
